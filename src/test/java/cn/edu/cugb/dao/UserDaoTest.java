5d1a30415ebd5255a9e20e4e64637a1682424cbb
210myg <129078677+ershimiyangguang@users.noreply.github.com>
2023-12-07 12:09:34 +0800

    编写功能文档 (#3)

--- src/test/java/cn/edu/cugb/dao/UserDaoTest.java
+++ src/test/java/cn/edu/cugb/dao/UserDaoTest.java
@@ -12,37 +12,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 public class UserDaoTest {
-    @Before
-    public void SetUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Test
-    public void DaoTest() throws Exception {
-        ApplicationContext applicationContext= new ClassPathXmlApplicationContext("xml/spring.xml");
-        JdbcTemplate jdbcTemplate = applicationContext.getBean("JdbcTemplate", JdbcTemplate.class);
-        String sql="select *from dish";
-        List<Dish> dishes = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Dish.class));
-        System.out.println(dishes);
+    public void UserDaoTest() throws Exception {
 
     }
-    @Test
-    public void UserTest(){
-        ApplicationContext applicationContext= new ClassPathXmlApplicationContext("xml/spring.xml");
-        JdbcTemplate jdbcTemplate = applicationContext.getBean("JdbcTemplate", JdbcTemplate.class);
-        String sql="select *from users";
-        List<User> dishes = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
-        System.out.println(dishes);
-    }
-   @Test
-    public void OrderTest(){
-       ApplicationContext applicationContext= new ClassPathXmlApplicationContext("xml/spring.xml");
-       JdbcTemplate jdbcTemplate = applicationContext.getBean("JdbcTemplate", JdbcTemplate.class);
-       String sql="select *from orders";
-       List<Order> dishes = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Order.class));
-       System.out.println(dishes);
-   }
 }
