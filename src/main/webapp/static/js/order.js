a079c3bfbf076f54bb9b53c2cdba6d9a7a94f5ce
210myg <129078677+ershimiyangguang@users.noreply.github.com>
2023-12-13 16:39:53 +0800

    清空购物车功能 (#34)

--- src/main/webapp/static/js/order.js
+++ src/main/webapp/static/js/order.js
@@ -126,4 +126,19 @@ function pay() {
     }
 }
 function clearCart() {
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials=true;
+    xhr.open("post", "CleanShoppingCartServlet", false);
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status == 200) {
+                let res = JSON.parse(xhr.responseText);
+                if (res.code==0) {
+                    loadShoppingCart();
+                }
+            }
+        }
+    }
+    xhr.send();
 }
