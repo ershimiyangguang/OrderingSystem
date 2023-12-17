d45d49e664e7a016b9a771d34ca49f90adb68962
ershimiyangguang <2740929986@qq.com>
2023-12-17 21:18:37 +0800

    修复注释错误

--- src/main/webapp/static/js/isAdminLogin.js
+++ src/main/webapp/static/js/isAdminLogin.js
@@ -1,15 +1,15 @@
-// window.addEventListener("load",()=>{
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("get", "IsAdminLoginServlet", true);
-//     xhr.onreadystatechange = () => {
-//         if (xhr.readyState === 4) {
-//             if (xhr.status == 200) {
-//                 let res = JSON.parse(xhr.responseText);
-//                 if (res.code==1) {
-//                     window.open("adminLogin.html","_self");
-//                 }
-//             }
-//         }
-//     }
-//     xhr.send();
-// })
+window.addEventListener("load",()=>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("get", "IsAdminLoginServlet", true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status == 200) {
+                let res = JSON.parse(xhr.responseText);
+                if (res.code==1) {
+                    window.open("adminLogin.html","_self");
+                }
+            }
+        }
+    }
+    xhr.send();
+})
